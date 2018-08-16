1dba289e3b754cda1ae3473776a22c2790cbb1bd
Ryan Casey <Ryan1@Stephen-Hawkings-Brain.local>
2018-08-15 19:51:09 -0600

    Updated assignment 'Bloc Jams–Range-inputs' to show correctly formatted time stamps for album playlists.

--- src/components/Album.js
+++ src/components/Album.js
@@ -153,7 +153,7 @@ class Album extends Component {
                         (<span className="song-number">{index + 1}</span>)}
                     </td>
                     <td>{song.title}</td>
-                    <td>{song.duration}</td>
+                    <td>{this.formatTime(song.duration)}</td>
                   </tr>
                 )
               }
